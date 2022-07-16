1287016a5db9d6c5715814f330dc7e4b8c8117ec
OtavioVB <otaviovb.developer@gmail.com>
2022-07-16 19:07:16 -0300

    fix(route): adicionado rotas de areas

---
+++ ForumArtigos/ForumArtigos/Areas/Home/Controllers/ArtigosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForumArtigos.Areas.Home.Controllers
+{
+    public class ArtigosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
